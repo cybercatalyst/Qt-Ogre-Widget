c7cd1c68ffce6480cf58aaa1684723bbca8aeb96
Jacob Dawid <jacob@omg-it.works>
2015-02-01 22:40:35 +0100

    Refactored and stabilized code.

--- QOgreWidget.cpp
+++ QOgreWidget.cpp
@@ -1,144 +1,154 @@
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// Own includes
 #include "QOgreWidget.h"
+
+// Qt includes
 #include <QX11Info>
+#include <QDebug>
 
-/**
- * @brief init the object
- * @author kito berg-taylor
- */
-void QOgreWidget::init( std::string plugins_file,
-         std::string ogre_cfg_file,
-         std::string ogre_log )
-{
-  // create the main ogre object
-  mOgreRoot = new Ogre::Root( plugins_file, ogre_cfg_file, ogre_log );
+bool QOgreWidget::initializeOgre(std::string plugins_file,
+                        std::string ogre_cfg_file,
+                        std::string ogre_log) {
+    // create the main ogre object
+    _ogreRoot = new Ogre::Root( plugins_file, ogre_cfg_file, ogre_log );
 
-  // setup a renderer
-  const Ogre::RenderSystemList& renderers = mOgreRoot->getAvailableRenderers();
-  assert( !renderers.empty() ); // we need at least one renderer to do anything useful
 
-  Ogre::RenderSystem* renderSystem = chooseRenderer( renderers );
+    // setup a renderer
+    const Ogre::RenderSystemList& renderers = _ogreRoot->getAvailableRenderers();
+    if(renderers.empty()) {
+        qDebug() << "No renderer available for OGRE.";
+        return false;
+    }
 
-  mOgreRoot->setRenderSystem( renderSystem );
-  QString dimensions = QString( "%1x%2" )
-                    .arg(this->width())
-                    .arg(this->height());
+    // initialize without creating window
+    Ogre::RenderSystem* renderSystem = chooseRenderer(renderers);
+    if(!renderSystem) {
+        qDebug() << "Supplied a null render system to OGRE.";
+        return false;
+    }
 
-  renderSystem->setConfigOption( "Video Mode", dimensions.toStdString() );
+    _ogreRoot->setRenderSystem(renderSystem);
+    _ogreRoot->getRenderSystem()->setConfigOption("Video Mode", QString("%1x%2").arg(width()).arg(height()).toStdString());
+    _ogreRoot->getRenderSystem()->setConfigOption("Full Screen", "No");
+    _ogreRoot->saveConfig();
 
-  // initialize without creating window
-  mOgreRoot->getRenderSystem()->setConfigOption( "Full Screen", "No" );
-  mOgreRoot->saveConfig();
-  mOgreRoot->initialise(false); // don't create a window
+     // don't create a window
+    _ogreRoot->initialise(false);
+    return true;
 }
 
-/**
- * @brief setup the rendering context
- * @author Kito Berg-Taylor
- */
-void QOgreWidget::initializeGL()
-{
-  //== Creating and Acquiring Ogre Window ==//
+void QOgreWidget::initializeGL() {
+    if(!initializeOgre("../bin/plugins.cfg", "../bin/ogre.cfg", "../bin/ogre.log")) {
+        return;
+    }
 
-  // Get the parameters of the window QT created
-  Ogre::String winHandle;
+    //== Creating and Acquiring Ogre Window ==//
+
+    // Get the parameters of the window QT created
+    Ogre::String windowHandle;
 #ifdef WIN32
-  // Windows code
-  winHandle += Ogre::StringConverter::toString((unsigned long)(this->parentWidget()->winId()));
+    // Windows code
+    windowHandle += Ogre::StringConverter::toString((unsigned long)(winId()));
 #elif MACOS
-  // Mac code, tested on Mac OSX 10.6 using Qt 4.7.4 and Ogre 1.7.3
-  Ogre::String winHandle  = Ogre::StringConverter::toString(winId());
+    // Mac code, tested on Mac OSX 10.6 using Qt 4.7.4 and Ogre 1.7.3
+    Ogre::String windowHandle  = Ogre::StringConverter::toString(winId());
 #else
-  // Unix code
-  winHandle  = Ogre::StringConverter::toString((unsigned long)(QX11Info::display()));
-  winHandle += ":";
-  winHandle += Ogre::StringConverter::toString((unsigned int)(QX11Info::appScreen()));
-  winHandle += ":";
-  winHandle += Ogre::StringConverter::toString((unsigned long)(this->parentWidget()->winId()));
+    // Unix code
+    windowHandle  = Ogre::StringConverter::toString((unsigned long)(QX11Info::display()));
+    windowHandle += ":";
+    windowHandle += Ogre::StringConverter::toString((unsigned int)(QX11Info::appScreen()));
+    windowHandle += ":";
+    windowHandle += Ogre::StringConverter::toString((unsigned long)(winId()));
 #endif
 
-
-  Ogre::NameValuePairList params;
+    Ogre::NameValuePairList params;
 #ifndef MACOS
-  // code for Windows and Linux
-  params["parentWindowHandle"] = winHandle;
-  mOgreWindow = mOgreRoot->createRenderWindow( "QOgreWidget_RenderWindow",
-                           this->width(),
-                           this->height(),
-                           false,
-                           &params );
+    // code for Windows and Linux
+    params["parentWindowHandle"] = windowHandle;
+    _ogreWindow = _ogreRoot->createRenderWindow( "QOgreWidget_RenderWindow",
+                                                 this->width(),
+                                                 this->height(),
+                                                 false,
+                                                 &params );
 
-  mOgreWindow->setActive(true);
-  WId ogreWinId = 0x0;
-  mOgreWindow->getCustomAttribute( "WINDOW", &ogreWinId );
+    _ogreWindow->setActive(true);
+    WId ogreWinId = 0x0;
+    _ogreWindow->getCustomAttribute( "WINDOW", &ogreWinId );
 
-  assert( ogreWinId );
+    if(!ogreWinId) {
+        qDebug() << "Ogre window id is null.";
+    }
 
-  // bug fix, extract geometry
-  QRect geo = this->frameGeometry ( );
-
-  // create new window
-  this->create( ogreWinId );
-
-  // set geometrie infos to new window
-  this->setGeometry (geo);
+    // bug fix, extract geometry
+    QRect geometry = this->frameGeometry();
+    // create new window
+    create(ogreWinId);
+    // set geometrie infos to new window
+    setGeometry(geometry);
 
 #else
-  // code for Mac
-  params["externalWindowHandle"] = winHandle;
-  params["macAPI"] = "cocoa";
-  params["macAPICocoaUseNSView"] = "true";
-  mOgreWindow = mOgreRoot->createRenderWindow("QOgreWidget_RenderWindow",
-      width(), height(), false, &params);
-  mOgreWindow->setActive(true);
-  makeCurrent();
+    // code for Mac
+    params["externalWindowHandle"] = windowHandle;
+    params["macAPI"] = "cocoa";
+    params["macAPICocoaUseNSView"] = "true";
+    _ogreWindow = _ogreRoot->createRenderWindow("QOgreWidget_RenderWindow",
+                                                width(), height(), false, &params);
+    _ogreWindow->setActive(true);
+    makeCurrent();
 #endif
 
-  setAttribute( Qt::WA_PaintOnScreen, true );
-  setAttribute( Qt::WA_NoBackground );
+    setAttribute( Qt::WA_PaintOnScreen, true );
+    setAttribute( Qt::WA_NoBackground );
 
-  //== Ogre Initialization ==//
-  Ogre::SceneType scene_manager_type = Ogre::ST_EXTERIOR_CLOSE;
+    //== Ogre Initialization ==//
+    Ogre::SceneType sceneType = Ogre::ST_EXTERIOR_CLOSE;
 
-  mSceneMgr = mOgreRoot->createSceneManager( scene_manager_type );
-  mSceneMgr->setAmbientLight( Ogre::ColourValue(1,1,1) );
+    _sceneManager = _ogreRoot->createSceneManager( sceneType );
+    _sceneManager->setAmbientLight( Ogre::ColourValue(1,1,1) );
 
-  mCamera = mSceneMgr->createCamera( "QOgreWidget_Cam" );
-  mCamera->setPosition( Ogre::Vector3(0,1,0) );
-  mCamera->lookAt( Ogre::Vector3(0,0,0) );
-  mCamera->setNearClipDistance( 1.0 );
+    _camera = _sceneManager->createCamera( "QOgreWidget_Cam" );
+    _camera->setPosition( Ogre::Vector3(0,1,0) );
+    _camera->lookAt( Ogre::Vector3(0,0,0) );
+    _camera->setNearClipDistance( 1.0 );
 
-  Ogre::Viewport *mViewport = mOgreWindow->addViewport( mCamera );
-  mViewport->setBackgroundColour( Ogre::ColourValue( 0.8,0.8,1 ) );
+    _viewport = _ogreWindow->addViewport(_camera);
+    _viewport->setBackgroundColour(Ogre::ColourValue( 0.8,0.8,1 ));
 }
 
-/**
- * @brief render a frame
- * @author Kito Berg-Taylor
- */
-void QOgreWidget::paintGL()
-{
-  // Be sure to call "OgreWidget->repaint();" to call paintGL
-  assert( mOgreWindow );
-  mOgreRoot->renderOneFrame();
+void QOgreWidget::paintGL() {
+    // Be sure to call "OgreWidget->repaint();" to call paintGL
+    if(_ogreRoot && _ogreWindow) {
+        _ogreRoot->renderOneFrame();
+    }
 }
 
-/**
- * @brief resize the GL window
- * @author Kito Berg-Taylor
- */
-void QOgreWidget::resizeGL( int width, int height )
-{
-  assert( mOgreWindow );
-  mOgreWindow->windowMovedOrResized();
+void QOgreWidget::resizeGL( int width, int height ) {
+    Q_UNUSED(width);
+    Q_UNUSED(height);
+    if(_ogreWindow) {
+        _ogreWindow->windowMovedOrResized();
+    }
 }
 
-/**
- * @brief choose the right renderer
- * @author Kito Berg-Taylor
- */
-Ogre::RenderSystem* QOgreWidget::chooseRenderer(const Ogre::RenderSystemList& renderers )
-{
-  // It would probably be wise to do something more friendly
-  // that just use the first available renderer
-  return *renderers.begin();
+Ogre::RenderSystem* QOgreWidget::chooseRenderer(const Ogre::RenderSystemList& renderers) {
+    // Default implementation chooses first renderer.
+    // Override to do something different.
+    return *renderers.begin();
 }
--- QOgreWidget.h
+++ QOgreWidget.h
@@ -1,43 +1,56 @@
-#ifndef __OGREWIDGET_H__
-#define __OGREWIDGET_H__
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 
+#pragma once
+
+// OGRE includes
 #include <OGRE/Ogre.h>
+
+// Qt includes
 #include <QGLWidget>
 #include <QX11Info>
 
-class QOgreWidget : public QGLWidget
-{
-  //Q_OBJECT;
-
- public:
-  QOgreWidget( QWidget *parent=0 ):
-    QGLWidget( parent ),
-    mOgreWindow(NULL)
-    {
-      init( "../bin/plugins.cfg", "../bin/ogre.cfg", "../bin/ogre.log" );
+class QOgreWidget : public QGLWidget {
+    Q_OBJECT
+public:
+    QOgreWidget(QWidget *parent = 0):
+        QGLWidget(parent),
+        _ogreWindow(0) {
     }
 
-  virtual ~QOgreWidget()
-    {
-      mOgreRoot->shutdown();
-      delete mOgreRoot;
-      destroy();
+    virtual ~QOgreWidget() {
+        _ogreRoot->shutdown();
+        delete _ogreRoot;
+        destroy();
     }
 
- protected:
-  virtual void initializeGL();
-  virtual void resizeGL( int, int );
-  virtual void paintGL();
+protected:
+    virtual void initializeGL();
+    virtual void resizeGL(int, int);
+    virtual void paintGL();
 
-  void init( std::string, std::string, std::string );
+    bool initializeOgre(std::string, std::string, std::string);
 
-  virtual Ogre::RenderSystem *chooseRenderer(const Ogre::RenderSystemList& );
+    virtual Ogre::RenderSystem *chooseRenderer(const Ogre::RenderSystemList& );
 
-  Ogre::Root *mOgreRoot;
-  Ogre::RenderWindow *mOgreWindow;
-  Ogre::Camera *mCamera;
-  Ogre::Viewport *mViewport;
-  Ogre::SceneManager *mSceneMgr;
+    Ogre::Root *        _ogreRoot;
+    Ogre::RenderWindow *_ogreWindow;
+    Ogre::Camera *      _camera;
+    Ogre::Viewport *    _viewport;
+    Ogre::SceneManager *_sceneManager;
 };
-
-#endif
